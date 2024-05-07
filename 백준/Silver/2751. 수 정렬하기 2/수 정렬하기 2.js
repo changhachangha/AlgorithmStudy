a72630699c9baade9bf1f681bde9b908fc8119a2
changhachangha <154064156+changhachangha@users.noreply.github.com>
2024-05-07 13:33:24 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1316 ms, Memory: 214728 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,8 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+input.shift();
+console.log(input.sort((a,b)=> a-b).join('\n'))
