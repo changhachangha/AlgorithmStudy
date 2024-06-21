81c68b989a097eb7137c0304278fc1e1164ea832
changhachangha <154064156+changhachangha@users.noreply.github.com>
2024-06-21 14:51:21 +0900

    [level 0] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,6 @@
+function solution(arr1, arr2) {
+    return arr1.length === arr2.length ? (
+        arr1.reduce((acc,cur) => acc + cur ,0) === arr2.reduce((acc,cur) => acc + cur ,0) ? 
+        0 : (arr1.reduce((acc,cur) => acc + cur ,0) > arr2.reduce((acc,cur) => acc + cur ,0) ? 1 : -1)
+    ) : (arr1.length > arr2.length ? 1 : -1)
+}
