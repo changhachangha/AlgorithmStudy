ea0b0605d283ba0680eeaf5be5e7bddc5656cf74
changhachangha <154064156+changhachangha@users.noreply.github.com>
2024-06-28 16:43:25 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,4 @@
+function solution(age) {
+    const alpAge = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z']
+    return age.toString().split('').map(v=>alpAge[v]).join('');
+}
